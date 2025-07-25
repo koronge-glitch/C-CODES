532635d600be1c952b4d5678f117d842896105ea
koronge-glitch <samsonkoronge24@gmail.com>
2025-07-25 16:21:30 +0300

    Add files via upload

---
+++ math project.cxx
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int product (int x, int y);
+float division (float a, float b);
+int addition (int x, int y){
+return x + y;
+}
+int intrest(int p,int t,int r){
+        return(p*t*r)/100;
+}
+
+int main(){
+        int sum,num1,num2;
+        cout<<"Funtions in C++"<<endl;
+        cout<<"Enter the first number"<<endl;
+        cin>>num1;
+        cout<<"Enter the second number"<<endl;
+        cin>>num2;
+        
+        sum = addition (num1,num2);
+        cout<<"Caling Addition"<<sum<<endl;
+
+
+        int multiplication;
+        multiplication=product (10, 20);
+        cout<<"Calling product"<<multiplication<<endl;
+
+        float marks;
+        marks=division (10, 30);
+        cout<<"Calling division"<<marks<<endl;
+
+
+        int simple,principle,rate,time;
+        cout<<"Enter the principle amount"<<endl;
+        cin>>principle;
+        cout<<"Enter the rate"<<endl;
+        cin>>rate;
+        cout<<"Enter your time"<<endl;
+        cin>>time;
+
+        simple=intrest(principle,2,rate);
+        cout<<"You inntrest is"<<simple<<endl;
+        return 0;
+
+
+
+}
+int product(int x, int y){
+        return x * y;
+}
+float division (float a, float b){
+        return a/b;
+}
